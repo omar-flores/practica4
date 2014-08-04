f7ce78d2f812b1cf5c72d4c60dcb95551e44148f
OMAR FLORES NIEVES <reydek-14@hotmail.com>
2014-08-04 09:46:08 -0500

    practica4
    plug in

---
+++ js/acciones.js
@@ -0,0 +1,13 @@
+//JavaScript
+	
+$(document).ready(function(e){
+	
+	$('#Beep').click(
+function(e){
+	alert('le acavas de dar clic al boton Beep')
+});//click continuar fecha
+	$('#Vibrar').click(
+function(e){
+	alert('le acavas de dar clic al boton Vibrar')
+});//click continuar fecha
+});//ready
