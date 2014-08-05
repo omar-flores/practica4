f8fe5dc57662a993a7deae82fca663063f89f1f1
omar flores <reydek-14@hotmail.com>
2014-08-05 08:59:26 -0500

    correcion
    de index  y del beep

--- js/acciones.js
+++ js/acciones.js
@@ -4,10 +4,14 @@ $(document).ready(function(e){
 	
 	$('#Beep').click(
 function(e){
-	navigator.notification.Beep(1);
+	navigator.notification.beep(1);
 });//click continuar fecha
 	$('#Vibrar').click(
 function(e){
 	navigator.notification.vibrate(1000);
+});//click continuar fecha
+	$('#play').click(
+function(e){
+	navigator.notification.play(1000);
 });//click continuar fecha
 });//ready
