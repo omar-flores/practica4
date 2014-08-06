5351db6f154eab72777cdaea06ce7a981a9c34bc
omar flores <reydek-14@hotmail.com>
2014-08-06 08:31:55 -0500

    correcion de sonido
    correcion de sonido de vibrar

--- js/acciones.js
+++ js/acciones.js
@@ -2,19 +2,21 @@
 	var audio;
 	
 $(document).ready(function(e){
-	if (windows.plugins && windows.plugins.lowlatencyAudio)
-	audio=windows.plugins.lowlatencyAudio;
-	audio.preoladFX('mario','audio/mario.mp3',function(msg){},function(msg){alert('error:' +msg);} )
-	$('#Beep').click(
-function(e){
-	navigator.notification.beep(1);
-});//click continuar fecha
-	$('#Vibrar').click(
-function(e){
+document.addEventListener("deviceready",function () {
+	audio= window.plugins.LowLatencyAudio;
+	audio.preloadFX('mario','audio/mario.mp3',function(msg){},function(msg){alert('error: ' +msg);});
+	
+	$ ('#Beep').bind ("tap",function (e)  { navigator.notification.beep(1);
+	});
+	
+	$ ('#play').bind ("tap", function (e) { audio.play ('mario');
+    	});//tap vibrar
+		
+	$('#Vibrar').bind("tap",
+    function(e){
 	navigator.notification.vibrate(1000);
-});//click continuar fecha
-	$('#play').click(
-function(e){
-	audio.play('mario')
-});//click continuar fecha
+	});//tap vibrar
+	
+
+});
 });//ready
